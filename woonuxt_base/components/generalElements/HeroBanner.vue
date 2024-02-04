75154563a6ab91eda2280d9c68a82e1dca426343
David | Wolf Worldwide <wolf.translations2021@gmail.com>
2024-02-04 12:32:17 +0100

    Testing if all goes well

--- woonuxt_base/components/generalElements/HeroBanner.vue
+++ woonuxt_base/components/generalElements/HeroBanner.vue
@@ -13,10 +13,10 @@
       fetchpriority="high"
       preload />
     <div class="container absolute inset-0 flex flex-col items-start justify-center bg-gradient-to-l from-gray-200 md:bg-none p-8">
-      <h1 class="text-3xl font-bold md:mb-4 md:text-4xl lg:text-6xl">Just landed.</h1>
-      <h2 class="text-lg font-bold md:mb-4 lg:text-3xl">The New Year Collection</h2>
+      <h1 class="text-3xl font-bold md:mb-4 md:text-4xl lg:text-6xl">De mooiste avonturen</h1>
+      <h2 class="text-lg font-bold md:mb-4 lg:text-3xl">beginnen met een pootafdruk naast die van jou.</h2>
       <div class="max-w-sm mb-8 text-md font-light lg:max-w-md text-balance">
-        <p>Our latest collection is here. Discover the latest trends and styles for the new year.</p>
+        <p>Elk huisdier heeft een verhaal; wij helpen je om het te vertellen.</p>
       </div>
       <NuxtLink class="px-6 py-3 font-bold text-white bg-gray-800 rounded-xl hover:bg-gray-800" to="/products">Shop now</NuxtLink>
     </div>
